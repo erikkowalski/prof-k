95936dfd247c8fbbc8f39336941302bf8688ccbc
erikkowalski <erikkowalski@mac.com>
2015-08-12 18:08:14 -0400

    Added social icons and copyright

--- templates/footer.php
+++ templates/footer.php
@@ -3,5 +3,12 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
 
   </div>
+	<div class="row social-icons container">
 
+		<div class="col-xs-12 col-sm-4  col-md-4"><div class="img-wrap"><a href="http://be.net/erikkowalski"><img src="_img/social-icons/behance.png"; alt="behance profile"></a></div></div>
+		<div class="col-xs-12 col-sm-4  col-md-4"><div class="img-wrap"><a href="www.linkedin.com/pub/erik-kowalski/7/883/ba5/"> <img src="_img/social-icons/linkedin.png"; alt="linkedin profile"></a></div></div>
+		<div class="col-xs-12 col-sm-4  col-md-4"> <div class="img-wrap"><a href="http://www.pinterest.com/proferik/graphic-design-typography/"> <img src="_img/social-icons/pinterest.png"; alt="pinterest page"></a></div></div>
+
+	</div>
+	<p>&copy; Professor Erik Kowalski <?php echo date("Y"); ?></p>
 </footer>
